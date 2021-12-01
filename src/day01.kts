faf6ad4be743478dc0775368282b1ee72ce1d0c7
Marquis Kurt <software@marquiskurt.net>
2021-12-01 17:04:46 -0500

    :tada: Initial commit - Day 1
    Signed-off-by: Marquis Kurt <software@marquiskurt.net>

---
+++ src/day01.kts
@@ -0,0 +1,84 @@
+import java.io.File
+
+fun getInputData(): List<Int> {
+    with (File("assets/day01.txt")) {
+        return this.readText()
+            .trim()
+            .split("\n")
+            .map(String::toInt)
+    }
+}
+
+fun getTestData(): List<Int> {
+    return """
+        199
+        200
+        208
+        210
+        200
+        207
+        240
+        269
+        260
+        263
+    """.trimIndent()
+        .split("\n")
+        .map(String::toInt)
+}
+
+
+fun List<Int>.getDeltaChanges(): Pair<Int, Int> {
+    var increased = 0; var decreased = 0
+    var previousValue = first()
+
+    drop(1).forEach { measurement ->
+        if (measurement > previousValue) {
+            increased += 1
+        } else {
+            decreased += 1
+        }
+        previousValue = measurement
+    }
+
+    return Pair(increased, decreased)
+}
+
+
+fun partOne(measurements: List<Int>): Int {
+    val increased = measurements.getDeltaChanges().first
+    println("Number of total increases from previous measurements: $increased")
+    return increased
+}
+
+fun partTwo(measurements: List<Int>): Int {
+    var slidingWindowStart = 0
+    var slidingWindowEnd = 2
+
+    val sums = mutableListOf<Int>()
+
+    while (slidingWindowEnd < measurements.count()) {
+        sums.add(measurements.slice(slidingWindowStart..slidingWindowEnd).sum())
+        slidingWindowStart += 1
+        slidingWindowEnd += 1
+    }
+
+    val increased = sums.getDeltaChanges().first
+    println("Number of total increases from previous sliding windows: $increased")
+    return increased
+}
+
+fun test() {
+    val testInput = getTestData()
+    println("=== Test Suite ===")
+    assert(partOne(testInput) == 7)
+    assert(partTwo(testInput) == 5)
+}
+
+fun main() {
+    val measurements = getInputData()
+    partOne(measurements)
+    partTwo(measurements)
+}
+
+test()
+main()
